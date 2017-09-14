3e09e52e12d6996fe7c0c0582a4e452f6b91664e
Ivan Rogov <xlazex@gmail.com>
2017-09-14 18:29:37 +0300

    trying to use heroku

--- app.js
+++ app.js
@@ -7,5 +7,16 @@
   "image": "heroku/nodejs",
   "scripts": {
     "start": "node index.js"
-  }
+  },
+  "dependencies": {
+    "body-parser": "^1.17.1",
+    "ejs": "^2.5.6",
+    "express": "^4.15.2",
+    "express-urlrewrite": "^1.2.0",
+    "generate-rsa-keypair": "^0.1.1",
+    "ioredis": "^3.0.0",
+    "lodash": "^4.17.4",
+    "mongodb": "^2.2.31",
+    "oidc-provider": "^2.0.0"
+  },
 }
