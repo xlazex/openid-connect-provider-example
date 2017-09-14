bc71cf1774c63f873f4ab3e577968a9be6602845
Ivan Rogov <xlazex@gmail.com>
2017-09-14 18:30:57 +0300

    gkgk

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 {
   "name": "Example OpenID Connect Provider",
-  "description": "",
+  "description": "Testing",
   "repository": "https://github.com/xlazex/openid-connect-provider-example",
   "logo": "https://cdn.rawgit.com/heroku/node-js-getting-started/master/public/node.svg",
   "keywords": ["node", "express", "heroku"],
