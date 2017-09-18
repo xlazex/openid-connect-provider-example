67c6c98af45179d451f8dbe719d617486e1e0b07
Ivan Rogov <xlazex@gmail.com>
2017-09-18 16:34:13 +0300

    utils -> generate keystore

---
+++ cliUtil/generateKeystore.js
@@ -0,0 +1,72 @@
+const Provider = require('oidc-provider')
+const keystore = Provider.createKeyStore()
+const fs = require('fs')
+const path = require('path')
+
+const consolePrefix = 'OIDCP/generateKeystore: '
+
+const progressTimer = (function() {
+  const L = ["\\", "|", "/", "-"]
+  const P = ["[__[]__]", "[_[__]_]", "[_[__]_]", "[__[]__]"]
+  var x = 0
+  return setInterval(function() {
+    process.stdout.write(`\r${consolePrefix} Generating: ${P[x++]}`)
+    x &= 3
+  }, 250)
+})()
+
+async function construct() {
+
+  console.log(`${consolePrefix}`, 'Starting...')
+
+  await Promise.all([ //@updates: keystore
+    keystore.generate('RSA', 2048, {
+      alg: 'RS256',
+      kid: 'signing-key',
+      use: 'sig',
+    }),
+    keystore.generate('RSA', 2048, {
+      kid: 'enc-rs-0',
+      use: 'enc',
+    }),
+    keystore.generate('EC', 'P-256', {
+      kid: 'sig-ec2-0',
+      use: 'sig',
+    }),
+    keystore.generate('EC', 'P-256', {
+      kid: 'enc-ec2-0',
+      use: 'enc',
+    }),
+    keystore.generate('EC', 'P-384', {
+      kid: 'sig-ec3-0',
+      use: 'sig',
+    }),
+    keystore.generate('EC', 'P-384', {
+      kid: 'enc-ec3-0',
+      use: 'enc',
+    }),
+    keystore.generate('EC', 'P-521', {
+      kid: 'sig-ec5-0',
+      use: 'sig',
+    }),
+    keystore.generate('EC', 'P-521', {
+      kid: 'enc-ec5-0',
+      use: 'enc',
+    })
+  ])
+
+  clearInterval(progressTimer)
+
+  console.log(`\n${consolePrefix}`, 'Keys generated: \n', keystore.all())
+
+  fs.writeFileSync(path.resolve('json/keystore.json'), JSON.stringify(keystore.toJSON(true), null, 2))
+
+  console.log(`\n${consolePrefix}`, 'Saved to file: ', '\"json/keystore.json\"')
+  console.log(`${consolePrefix}`, 'Bye.')
+
+}
+
+construct()
+  .catch(err => {
+    console.error(`\n${consolePrefix}`, 'Error: ', err);
+  })
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const assert = require('assert')
 const express = require('express')
 const Provider = require('oidc-provider')
-const fs = require('fs')
-const path = require('path')
 // \Import
 
 // init and run
@@ -15,46 +13,7 @@ async function construct() {
     const mainConfig = require('./config/mainConfig')
     const providerConfig = require('./config/providerConfig')
     const providerSettings = require('./config/providerSettings')
-    const keystore = Provider.createKeyStore()
     const app = express()
-
-    // await Promise.all([ //@updates: keystore
-    //   keystore.generate('RSA', 2048, {
-    //     alg: 'RS256',
-    //     kid: 'signing-key',
-    //     use: 'sig',
-    //   }),
-    //   keystore.generate('RSA', 2048, {
-    //     kid: 'enc-rs-0',
-    //     use: 'enc',
-    //   }),
-    //   keystore.generate('EC', 'P-256', {
-    //     kid: 'sig-ec2-0',
-    //     use: 'sig',
-    //   }),
-    //   // keystore.generate('EC', 'P-256', {
-    //   //   kid: 'enc-ec2-0',
-    //   //   use: 'enc',
-    //   // }),
-    //   // keystore.generate('EC', 'P-384', {
-    //   //   kid: 'sig-ec3-0',
-    //   //   use: 'sig',
-    //   // }),
-    //   // keystore.generate('EC', 'P-384', {
-    //   //   kid: 'enc-ec3-0',
-    //   //   use: 'enc',
-    //   // }),
-    //   // keystore.generate('EC', 'P-521', {
-    //   //   kid: 'sig-ec5-0',
-    //   //   use: 'sig',
-    //   // }),
-    //   // keystore.generate('EC', 'P-521', {
-    //   //   kid: 'enc-ec5-0',
-    //   //   use: 'enc',
-    //   // })
-    // ])
-
-    //fs.writeFileSync(path.resolve('json/keystore.json'), JSON.stringify(keystore.toJSON(true), null, 2))
 
     const ks = require('./json/keystore.json');
 
